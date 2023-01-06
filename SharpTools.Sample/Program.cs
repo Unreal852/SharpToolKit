73cda17ef8b8251e785358712120c103f10ad5b3
Unreal852 <unreal852@hotmail.fr>
2023-01-06 13:50:31 +0100

    Whooops i forgot to remove debug code

--- SharpTools.Sample/Program.cs
+++ SharpTools.Sample/Program.cs
@@ -1,8 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Diagnostics;
 using SharpTools.Extensions;
-using SharpTools.Timing;
 
 namespace SharpTools.Sample;
 
@@ -10,26 +8,9 @@ internal static class Program
 {
     private static void Main(string[] args)
     {
-        var start = Stopwatch.GetTimestamp();
-        Thread.Sleep(1000);
-        var end = Stopwatch.GetElapsedTime(start);
-        Console.WriteLine($"Took: {end.TotalSeconds}");
-
-        // var start = Stopwatch.GetTimestamp();
-        // TimeSince2 since = 0;
-        // // ReSharper disable once LoopVariableIsNeverChangedInsideLoop
-        // while (since < 1.5)
-        // {
-        // }
-        //
-        // var end = Stopwatch.GetElapsedTime(start);
-        //
-        // Console.WriteLine($"Took: {end.TotalSeconds}");
-        //
-        //
-        // foreach (var i in 5..10)
-        // {
-        //     Console.WriteLine(i);
-        // }
+        foreach (var i in 5..10)
+        {
+            Console.WriteLine(i);
+        }
     }
 }
